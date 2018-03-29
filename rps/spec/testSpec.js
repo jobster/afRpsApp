3bd8ebdc7e66309a928f2a73a0a1666050aa653e
Chris Jobst <cjobst@pivotal.io>
2018-03-29 12:28:14 -0700

    Example of spec working

---
+++ rps/spec/testSpec.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+
+var expect = chai.expect;
+
+describe("testing specs", function() {
+    it("shows that tests work", function () {
+        expect(true).to.be.true;
+    });
+});
